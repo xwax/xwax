3ef5a94dbbe3341566838b46f4d36903d175cfa3
Mark Hills <mark@pogo.org.uk>
2010-03-27 15:04:35 +0000

    Update copyright

--- lut.c
+++ lut.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Mark Hills <mark@pogo.org.uk>
+ * Copyright (C) 2010 Mark Hills <mark@pogo.org.uk>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
