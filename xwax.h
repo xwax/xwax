14b00c5668cb3475724e46002cce2257a284c212
Mark Hills <mark@pogo.org.uk>
2010-08-18 22:13:57 +0100

    xwax 0.8-beta1

--- xwax.h
+++ xwax.h
@@ -20,7 +20,7 @@
 #ifndef XWAX_H
 #define XWAX_H
 
-#define BANNER "xwax 0.7 (C) Copyright 2010 Mark Hills <mark@pogo.org.uk>"
+#define BANNER "xwax 0.8-beta1 (C) Copyright 2010 Mark Hills <mark@pogo.org.uk>"
 
 #define NOTICE \
   "This software is supplied WITHOUT ANY WARRANTY; without even the implied\n"\
