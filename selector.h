21ba6d3acf05d371126a9d009fd28449e3c3aaa6
Mark Hills <mark@pogo.org.uk>
2011-01-03 19:54:56 +0000

    Update copyright notice

--- selector.h
+++ selector.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Mark Hills <mark@pogo.org.uk>,
+ * Copyright (C) 2011 Mark Hills <mark@pogo.org.uk>,
  *                    Yves Adler <yves.adler@googlemail.com>
  *
  * This program is free software; you can redistribute it and/or
