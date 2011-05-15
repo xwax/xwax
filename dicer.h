a7f07d48cb0a162007b37fe0066bc6730c9b3fcc
Mark Hills <mark@pogo.org.uk>
2011-05-15 23:02:57 +0100

    dicer: lInitial support for Novation Dicer
    The only controls are simple setting and seeking to cue points.
    No LEDs are updated.

---
+++ dicer.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2011 Mark Hills <mark@pogo.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
+#ifndef DICER_H
+#define DICER_H
+
+struct controller_t;
+struct rt_t;
+
+int dicer_init(struct controller_t *c, struct rt_t *rt, const char *hw);
+
+#endif
