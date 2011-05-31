46a7d4ae913c15fe7642be460ec1e40952136e02
Mark Hills <mark@pogo.org.uk>
2011-05-31 23:35:51 +0100

    dicer: Display status in output LEDs
    We assume the a single Dicer is the only device operating the cue
    points and so it sets its own LEDs to indicate whether a cue is set
    or not.

--- dicer.c
+++ dicer.c
@@ -26,6 +26,8 @@
 #include "midi.h"
 #include "realtime.h"
 
+#define NBUTTONS 5
+
 #define CUE  0
 #define LOOP 1
 #define ROLL 2
@@ -36,9 +38,21 @@ static const char *actions[] = {
     "ROLL"
 };
 
+/* LED states */
+
+typedef unsigned char led_t;
+
+#define ON      0x1
+#define PRESSED 0x2
+#define SYNCED  0x4
+
 struct dicer {
     struct midi midi;
     struct deck *left, *right;
+    led_t left_led[NBUTTONS], right_led[NBUTTONS];
+
+    char obuf[180];
+    size_t ofill;
 };
 
 /*
@@ -63,22 +77,222 @@ static int add_deck(struct controller *c, struct deck *k)
     return 0;
 }
 
-static void event_decoded(struct deck *d, unsigned char action,
-                          bool shift, unsigned char button, bool on)
+/*
+ * Write a MIDI command sequence which would bring the given LED
+ * up-to-date
+ *
+ * Return: n, or -1 if not enough buffer space
+ * Post: if buffer space is available, n bytes are written to buf
+ */
+
+static ssize_t led_cmd(led_t led, char *buf, size_t len,
+                       bool right, unsigned char action,
+                       bool shift, unsigned char button)
 {
+    if (len < 3)
+        return -1;
+
+    assert(action <= ROLL);
+    buf[0] = (right ? 0x9d : 0x9a) + action;
+
+    assert(button < NBUTTONS);
+    buf[1] = (shift ? 0x41 : 0x3c) + button;
+
+    /* The Dicer allows us to use any colour in any mode. For
+     * simplicity, we tie the colour to the mode at this layer */
+
+    switch (action) {
+    case CUE:
+        buf[2] = 0x00;
+        break;
+
+    case LOOP:
+        buf[2] = 0x70;
+        break;
+
+    case ROLL:
+        buf[2] = 0x40;
+        break;
+
+    default:
+        abort();
+    }
+
+    if (led & ON)
+        buf[2] += 0xa;
+    if (led & PRESSED)
+        buf[2] += 0x5;
+
+    debug("dicer: compiling LED command: %02hhx %02hhx %02hhx\n",
+          buf[0], buf[1], buf[2]);
+
+    return 3;
+}
+
+/*
+ * Push control code for a particular output LED
+ */
+
+static ssize_t sync_one_led(led_t *led, char *buf, size_t len,
+                            bool right, unsigned char button)
+{
+    unsigned int a;
+    size_t t;
+
+    if (*led & SYNCED)
+        return 0;
+
+    debug("dicer: syncing LED: %s %d\n", right ? "right" : "left", button);
+
+    /* For simplicify we light all LEDs in all modes the same:
+     * (cue, loop, roll) x (shift, non-shift) */
+
+    t = 0;
+
+    for (a = 0; a <= ROLL; a++) {
+        ssize_t z;
+
+        z = led_cmd(*led, buf, len, right, a, false, button);
+        if (z == -1)
+            return -1;
+
+        buf += z;
+        len -= z;
+        t += z;
+
+        z = led_cmd(*led, buf, len, right, a, true, button);
+        if (z == -1)
+            return -1;
+
+        buf += z;
+        len -= z;
+        t += z;
+    }
+
+    *led |= SYNCED;
+
+    return t;
+}
+
+/*
+ * Return: number of bytes written to the buffer
+ */
+
+static size_t sync_one_dicer(led_t led[NBUTTONS], bool right,
+                             char *buf, size_t len)
+{
+    size_t n, t;
+
+    t = 0;
+
+    for (n = 0; n < NBUTTONS; n++) {
+        ssize_t z;
+
+        z = sync_one_led(&led[n], buf, len, right, n);
+        if (z == -1) {
+            debug("dicer: output buffer full; expect incorrect LEDs\n");
+            break;
+        }
+
+        buf += z;
+        len -= z;
+        t += z;
+    }
+
+    return t;
+}
+
+/*
+ * Write a MIDI command sequence to sync all hardware LEDs with the
+ * state held in memory.
+ *
+ * The Dicer first appears to only have five output LEDs on two
+ * controllers. But there are three modes for each, and then shift
+ * on/off modes: total (5 * 2 * 3 * 2) = 60
+ */
+
+static void sync_all_leds(struct dicer *d)
+{
+    size_t w;
+    char *buf;
+    size_t len;
+
+    buf = d->obuf + d->ofill;
+    len = sizeof(d->obuf) - d->ofill;
+
+    /* Top-up the buffer, even if not empty */
+
+    w = sync_one_dicer(d->left_led, false, buf, len);
+    buf += w;
+    len -= w;
+    d->ofill += w;
+
+    w = sync_one_dicer(d->right_led, true, buf, len);
+    buf += w;
+    len -= w;
+    d->ofill += w;
+
+    if (d->ofill > 0) {
+        ssize_t z;
+
+        debug("dicer: Writing %zd bytes of MIDI command\n", d->ofill);
+
+        z = midi_write(&d->midi, d->obuf, d->ofill);
+        if (z == -1)
+            return;
+
+        if (z < d->ofill)
+            memmove(d->obuf, d->obuf + z, z);
+
+        d->ofill -= z;
+    }
+}
+
+/*
+ * Modify state flags of an LED
+ */
+
+static void set_led(led_t *led, unsigned char set, unsigned char clear)
+{
+    led_t n;
+
+    n = *led & ~clear | set;
+    if (n != *led)
+        *led = n & ~SYNCED;
+}
+
+/*
+ * Act on an event, and update the given LED status
+ */
+
+static void event_decoded(struct deck *d, led_t led[NBUTTONS],
+                          unsigned char action, bool shift,
+                          unsigned char button, bool on)
+{
+    /* Always toggle the LED status */
+
+    if (on) {
+        set_led(&led[button], PRESSED, 0);
+    } else {
+        set_led(&led[button], 0, PRESSED);
+    }
+
     if (!on)
         return;
 
     if (shift) {
         deck_unset_cue(d, button);
+        set_led(&led[button], 0, ON);
     } else {
         deck_cue(d, button);
+        set_led(&led[button], ON, 0);
     }
 }
 
 static void event(struct dicer *d, unsigned char buf[3])
 {
     struct deck *deck;
+    led_t *led;
     unsigned char action, button;
     bool on, shift;
 
@@ -92,6 +306,7 @@ static void event(struct dicer *d, unsigned char buf[3])
     case 0x9b:
     case 0x9c:
         deck = d->left;
+        led = d->left_led;
         action = buf[0] - 0x9a;
         break;
 
@@ -99,15 +314,13 @@ static void event(struct dicer *d, unsigned char buf[3])
     case 0x9e:
     case 0x9f:
         deck = d->right;
+        led = d->right_led;
         action = buf[0] - 0x9d;
         break;
 
     default:
         abort();
     }
-
-    if (deck == NULL)
-        return;
 
     switch (buf[1]) {
     case 0x3c:
@@ -151,7 +364,7 @@ static void event(struct dicer *d, unsigned char buf[3])
           button, on ? "ON" : "OFF",
           deck);
 
-    event_decoded(deck, action, shift, button, on);
+    event_decoded(deck, led, action, shift, button, on);
 }
 
 static int realtime(struct controller *c)
@@ -166,12 +379,16 @@ static int realtime(struct controller *c)
         if (z == -1)
             return -1;
         if (z == 0)
-            return 0;
+            break;
 
         debug("dicer: got event\n");
 
         event(d, buf);
     }
+
+    sync_all_leds(d);
+
+    return 0;
 }
 
 static void clear(struct controller *c)
@@ -191,6 +408,7 @@ static struct controller_ops dicer_ops = {
 
 int dicer_init(struct controller *c, struct rt *rt, const char *hw)
 {
+    size_t n;
     struct dicer *d;
 
     debug("dicer: init\n");
@@ -206,6 +424,10 @@ int dicer_init(struct controller *c, struct rt *rt, const char *hw)
 
     d->left = NULL;
     d->right = NULL;
+    d->ofill = 0;
+
+    for (n = 0; n < NBUTTONS; n++)
+        d->left_led[n] = 0;
 
     controller_init(c, &dicer_ops);
     c->local = d;
