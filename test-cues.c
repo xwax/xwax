d992f84e8dc67b92c68fd2ae3f13fe4dc374a6a9
Mark Hills <mark@pogo.org.uk>
2012-01-31 10:29:49 +0000

    Repair cue point test

--- test-cues.c
+++ test-cues.c
@@ -9,16 +9,14 @@
 
 int main(int argc, char *argv[])
 {
-    struct cues_t q;
+    struct cues q;
 
-    cues_init(&q);
+    cues_reset(&q);
 
     assert(cues_get(&q, 0) == CUE_UNSET);
 
     cues_set(&q, 0, 100.0);
     assert(cues_get(&q, 0) == 100.0);
-
-    cues_clear(&q);
 
     return 0;
 }
