10928a3f4db4f6aaeebcbf4ae0be9098dd1a4de2
Mark Hills <mark@xwax.org>
2014-01-15 21:05:01 +0000

    Update copyright notice
    Happy New Year!

--- selector.c
+++ selector.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Mark Hills <mark@xwax.org>,
+ * Copyright (C) 2014 Mark Hills <mark@xwax.org>,
  *                    Yves Adler <yves.adler@googlemail.com>
  *
  * This program is free software; you can redistribute it and/or
