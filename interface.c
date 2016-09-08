18e998d64b9f052960d1e45dcfdb95343ad2a85a
Daniel James <daniel@64studio.com>
2016-09-08 09:29:29 +0100

    Use left shift key to toggle record sort order

--- interface.c
+++ interface.c
@@ -1425,7 +1425,7 @@ static bool handle_key(SDLKey key, SDLMod mod)
         selector_rescan(sel);
         return true;
 
-    } else if (key == SDLK_LALT) {
+    } else if (key == SDLK_LSHIFT) {
         selector_toggle_order(sel);
         return true;
 
