593cbb1743fd444bdf4229a5aec5c3c02c804cae
Mark Hills <mark@xwax.org>
2021-08-18 21:29:21 +0100

    Move to GPL version 3
    When xwax was first released, this license didn't exist. Now it's
    desirable only for compatibility with other projects (notably Mixxx)
    which need to combine xwax code with other software, where the GPL
    version 2 is not compatible with version 3.
    
    This is "version 3" and not "version 3 or any later version".

--- tests/status.c
+++ tests/status.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2021 Mark Hills <mark@xwax.org>
+ *
+ * This file is part of "xwax".
+ *
+ * "xwax" is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * "xwax" is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <stdio.h>
 
 #include "status.h"
