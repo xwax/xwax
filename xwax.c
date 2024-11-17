c9bd1a2ea402478dde7847b4b715689648f24605
Mark Hills <mark@xwax.org>
2024-11-17 19:11:54 +0000

    Long command line flag for --rtprio

--- xwax.c
+++ xwax.c
@@ -76,7 +76,7 @@ static void usage(FILE *fd)
 
     fprintf(fd, "Program-wide options:\n"
       "  -k             Lock real-time memory into RAM\n"
-      "  -q <n>         Real-time priority (0 for no priority, default %d)\n"
+      "  --rtprio <n>   Real-time priority (0 for no priority, default %d)\n"
       "  --geometry <s>  Set display geometry (see man page)\n"
       "  --no-decor     Request a window with no decorations\n"
       "  -h             Display this message to stdout and exit\n\n",
@@ -271,6 +271,7 @@ int main(int argc, const char *argv[])
         deprecated(&argv[0], "-d", "--oss");
         deprecated(&argv[0], "-g", "--geometry");
         deprecated(&argv[0], "-j", "--jack");
+        deprecated(&argv[0], "-q", "--rtprio");
         deprecated(&argv[0], "-t", "--timecode");
 
         if (!strcmp(argv[0], "-h")) {
